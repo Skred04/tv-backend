7113a85bccd04afd1028fb5dfd4c47df75fdfa05
Pier Philippon <pier.philippon@gmail.com>
2021-02-06 19:38:27 +0100

    commit pour utilisation provider

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: "https://skred-tv-backend.herokuapp.com",
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '53da8617c0ee7873519956a4f8c642f3'),
